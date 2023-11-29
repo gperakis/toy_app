52303932a96a68d27a3faa4b952a9c6da39a519c
George Perakis <george.perakis@aisera.com>
2023-11-29 20:45:22 +0200

    Create dummy SQL scripts

---
+++ sql-scripts/02-insert-data.sql
@@ -0,0 +1,5 @@
+USE SampleDB;
+
+INSERT INTO users (username, email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO users (username, email) VALUES ('Bob', 'bob@example.com');
+INSERT INTO users (username, email) VALUES ('Charlie', 'charlie@example.com');
